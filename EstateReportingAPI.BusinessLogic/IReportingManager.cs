d72d241a93a06356885c387c3cd552f560da5901
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2023-10-10 18:19:25 +0100

    :|

--- EstateReportingAPI.BusinessLogic/IReportingManager.cs
+++ EstateReportingAPI.BusinessLogic/IReportingManager.cs
@@ -11,16 +11,13 @@ public interface IReportingManager{
     Task<LastSettlement> GetLastSettlement(Guid estateId, CancellationToken cancellationToken);
     Task<List<Merchant>> GetMerchants(Guid estateId, CancellationToken cancellationToken);
     Task<MerchantKpi> GetMerchantsTransactionKpis(Guid estateId, CancellationToken cancellationToken);
+    Task<List<Operator>> GetOperators(Guid estateId, CancellationToken cancellationToken);
     Task<TodaysSales> GetTodaysFailedSales(Guid estateId, DateTime comparisonDate, String responseCode, CancellationToken cancellationToken);
     Task<TodaysSales> GetTodaysSales(Guid estateId, DateTime comparisonDate, CancellationToken cancellationToken);
     Task<List<TodaysSalesCountByHour>> GetTodaysSalesCountByHour(Guid estateId, DateTime comparisonDate, CancellationToken cancellationToken);
     Task<List<TodaysSalesValueByHour>> GetTodaysSalesValueByHour(Guid estateId, DateTime comparisonDate, CancellationToken cancellationToken);
     Task<TodaysSettlement> GetTodaysSettlement(Guid estateId, DateTime comparisonDate, CancellationToken cancellationToken);
     Task<List<TopBottomData>> GetTopBottomData(Guid estateId, TopBottom direction, Int32 resultCount, Dimension dimension, CancellationToken cancellationToken);
-
-    Task<List<Merchant>> GetMerchants(Guid estateId, CancellationToken cancellationToken);
-
-    Task<List<Operator>> GetOperators(Guid estateId, CancellationToken cancellationToken);
 
     #endregion
 }
