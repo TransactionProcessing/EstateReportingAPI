5c351af0a909ec8ed4438665831dea92ccc90623
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2023-10-10 21:40:34 +0100

    Added Pending figures to Todays Settlement

--- EstateReportingAPI.IntegrationTests/FactSettlementsControllerTests.cs
+++ EstateReportingAPI.IntegrationTests/FactSettlementsControllerTests.cs
@@ -9,50 +9,32 @@
 
     public class FactSettlementsControllerTests : ControllerTestsBase, IDisposable
     {
-        //public void Dispose()
-        //{
-        //    EstateManagementGenericContext context = new EstateManagementSqlServerContext(GetLocalConnectionString($"EstateReportingReadModel{this.TestId.ToString()}"));
-
-        //    Console.WriteLine($"About to delete database EstateReportingReadModel{this.TestId.ToString()}");
-        //    Boolean result = context.Database.EnsureDeleted();
-        //    Console.WriteLine($"Delete result is {result}");
-        //    result.ShouldBeTrue();
-        //}
-
         [Fact]
         public async Task FactSettlementsController_TodaysSettlement_SettlementReturned(){
             EstateManagementGenericContext context = new EstateManagementSqlServerContext(GetLocalConnectionString($"EstateReportingReadModel{this.TestId.ToString()}"));
 
             DatabaseHelper helper = new DatabaseHelper(context);
-            List<(Decimal feeValue, Decimal calulatedValue, Int32 transactionFeeReportingId)> todaysSettlementFees = new();
-
             Int32 todaysSettlementTransactionCount = 15;
-            for (int i = 1; i <= todaysSettlementTransactionCount; i++){
-                todaysSettlementFees.Add((0.5m, 0.5m * i, i));
-            }
+            Int32 todaysPendingSettlementTransactionCount = 9;
+            var todayTotals = await helper.AddSettlementRecord(DateTime.Now, 1, 1, todaysSettlementTransactionCount, todaysPendingSettlementTransactionCount);
 
-            await helper.AddSettlementRecordWithFees(DateTime.Now, 1, 1, todaysSettlementFees);
+            Int32 comparisonSettlementTransactionCount = 15;
+            Int32 comparisonPendingSettlementTransactionCount = 9;
+            var comparisonTotals = await helper.AddSettlementRecord(DateTime.Now.AddDays(-1), 1, 1, todaysSettlementTransactionCount, todaysPendingSettlementTransactionCount);
 
-            List<(Decimal feeValue, Decimal calulatedValue, Int32 transactionFeeReportingId)> comparisonDateSettlementFees = new();
-
-            Int32 comparisonDateSettlementTransactionCount = 9;
-            for (int i = 1; i <= comparisonDateSettlementTransactionCount; i++)
-            {
-                comparisonDateSettlementFees.Add((0.5m, 0.5m * i, i));
-            }
-
-            DateTime comparisonDate = DateTime.Now.AddDays(-1);
-            await helper.AddSettlementRecordWithFees(comparisonDate, 1, 1, comparisonDateSettlementFees);
-
-            HttpResponseMessage response = await this.CreateAndSendHttpRequestMessage($"api/facts/settlements/todayssettlement?comparisonDate={comparisonDate.ToString("yyyy-MM-dd")}");
+            HttpResponseMessage response = await this.CreateAndSendHttpRequestMessage($"api/facts/settlements/todayssettlement?comparisonDate={DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd")}");
 
             String content = await response.Content.ReadAsStringAsync(CancellationToken.None);
             TodaysSettlement? todaysSettlement = JsonConvert.DeserializeObject<TodaysSettlement>(content);
-            todaysSettlement.ComparisonSettlementCount.ShouldBe(comparisonDateSettlementTransactionCount);
-            todaysSettlement.ComparisonSettlementValue.ShouldBe(comparisonDateSettlementFees.Sum(c => c.calulatedValue));
-
+            todaysSettlement.ComparisonSettlementCount.ShouldBe(comparisonSettlementTransactionCount);
+            todaysSettlement.ComparisonSettlementValue.ShouldBe(comparisonTotals.settlementFeesValue);
+            todaysSettlement.ComparisonPendingSettlementCount.ShouldBe(comparisonPendingSettlementTransactionCount);
+            todaysSettlement.ComparisonPendingSettlementValue.ShouldBe(comparisonTotals.pendingSettlementFeesValue);
+            
             todaysSettlement.TodaysSettlementCount.ShouldBe(todaysSettlementTransactionCount);
-            todaysSettlement.TodaysSettlementValue.ShouldBe(todaysSettlementFees.Sum(c => c.calulatedValue));
+            todaysSettlement.TodaysSettlementValue.ShouldBe(todayTotals.settlementFeesValue);
+            todaysSettlement.TodaysPendingSettlementCount.ShouldBe(todaysPendingSettlementTransactionCount);
+            todaysSettlement.TodaysPendingSettlementValue.ShouldBe(todayTotals.pendingSettlementFeesValue);
         }
 
         [Fact]
@@ -61,38 +43,23 @@
             EstateManagementGenericContext context = new EstateManagementSqlServerContext(GetLocalConnectionString($"EstateReportingReadModel{this.TestId.ToString()}"));
 
             DatabaseHelper helper = new DatabaseHelper(context);
-            List<(Decimal feeValue, Decimal calulatedValue, Int32 transactionFeeReportingId)> todaysSettlementFees = new();
-            List<Transaction> todaysSettlementTransactions = new();
-            List<Transaction> yeterdaysSettlementTransactions = new();
-            Int32 todaysSettlementTransactionCount = 15;
-            for (int i = 1; i <= todaysSettlementTransactionCount; i++){
-                var transaction = await helper.AddTransaction(DateTime.Now, 1, "Safaricom", 1, "0000", i);
-                todaysSettlementTransactions.Add(transaction);
-                todaysSettlementFees.Add((0.5m, 0.5m * i, i));
-            }
-            await helper.AddSettlementRecordWithFees(DateTime.Now, 1, 1, todaysSettlementFees);
-
-            List<(Decimal feeValue, Decimal calulatedValue, Int32 transactionFeeReportingId)> yesterdaysSettlementFees = new();
-
-            DateTime comparisonDate = DateTime.Now.AddDays(-1);
-            Int32 yesterdaysSettlementTransactionCount = 9;
-            for (int i = 1; i <= yesterdaysSettlementTransactionCount; i++)
-            {
-                var transaction = await helper.AddTransaction(comparisonDate, 1, "Safaricom", 1, "0000", i);
-                yeterdaysSettlementTransactions.Add(transaction);
-                yesterdaysSettlementFees.Add((0.5m, 0.5m * i, i));
-            }
             
-            await helper.AddSettlementRecordWithFees(comparisonDate, 1, 1, yesterdaysSettlementFees);
+            Int32 todaysSettlementTransactionCount = 15;
+            Int32 todaysPendingSettlementTransactionCount = 9;
+            var todayTotals = await helper.AddSettlementRecord(DateTime.Now, 1, 1, todaysSettlementTransactionCount, todaysPendingSettlementTransactionCount);
+
+            Int32 comparisonSettlementTransactionCount = 15;
+            Int32 comparisonPendingSettlementTransactionCount = 9;
+            var comparisonTotals = await helper.AddSettlementRecord(DateTime.Now.AddDays(-1), 1, 1, todaysSettlementTransactionCount, todaysPendingSettlementTransactionCount);
 
             HttpResponseMessage response = await this.CreateAndSendHttpRequestMessage($"api/facts/settlements/lastsettlement");
 
             response.IsSuccessStatusCode.ShouldBeTrue();
             String content = await response.Content.ReadAsStringAsync(CancellationToken.None);
             var lastSettlement = JsonConvert.DeserializeObject<LastSettlement>(content);
-            lastSettlement.FeesValue.ShouldBe(todaysSettlementFees.Sum(f => f.calulatedValue));
+            lastSettlement.FeesValue.ShouldBe(todayTotals.settlementFeesValue);
             lastSettlement.SalesCount.ShouldBe(todaysSettlementTransactionCount);
-            lastSettlement.SalesValue.ShouldBe(todaysSettlementTransactions.Sum(t => t.TransactionAmount));
+            lastSettlement.SalesValue.ShouldBe(todayTotals.settledTransactionsValue);
         }
     }
 }
