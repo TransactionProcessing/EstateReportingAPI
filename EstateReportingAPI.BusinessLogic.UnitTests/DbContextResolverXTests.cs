cd349dc1a24db953dbddc60e0cffb47b6869681d
StuartFerguson <stuart_ferguson1development@outlook.com>
2026-08-25 10:43:07 +0100

    Fix estate-specific resolver connection string

---
+++ EstateReportingAPI.BusinessLogic.UnitTests/DbContextResolverXTests.cs
@@ -0,0 +1,40 @@
+using EstateReportingAPI.BusinessLogic;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.EntityFramework;
+using Shared.General;
+using Shouldly;
+using TransactionProcessor.Database.Contexts;
+
+namespace EstateReportingAPI.BusinessLogic.UnitTests;
+
+public class DbContextResolverXTests
+{
+    [Fact]
+    public void Resolve_WhenEstateSuffixIsProvided_UsesTheSuffixedCatalog()
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["ConnectionStrings:TransactionProcessorReadModel"] = "Server=localhost;Initial Catalog=TransactionProcessorReadModel;Integrated Security=True;TrustServerCertificate=True"
+            })
+            .Build();
+
+        ConfigurationReader.Initialise(configuration);
+
+        using ServiceProvider rootProvider = new ServiceCollection().BuildServiceProvider();
+        DbContextResolverX<EstateManagementContext> resolver =
+            new(rootProvider, configuration, new QueryTimingInterceptor());
+
+        using ResolvedDbContext<EstateManagementContext> resolvedContext =
+            resolver.Resolve("TransactionProcessorReadModel", "estate-123");
+
+        SqlConnectionStringBuilder connectionStringBuilder =
+            new(resolvedContext.Context.Database.GetDbConnection().ConnectionString);
+
+        connectionStringBuilder.InitialCatalog.ShouldBe("TransactionProcessorReadModel-estate-123");
+    }
+}
